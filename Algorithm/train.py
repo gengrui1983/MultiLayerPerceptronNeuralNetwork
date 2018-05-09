0cab35ba4c0b60ee3995c74ed00d813b14074b86
gengrui1983 <r.geng@foxmail.com>
2018-05-09 21:33:50 +1000

    - final results.

--- Algorithm/train.py
+++ Algorithm/train.py
@@ -20,18 +20,18 @@ X_val = data.training_val - mean / std
 X_test = data.testing - mean / std
 
 batch_size = 200
-max_epoch = 300
+max_epoch = 1000
 
-nn = MLP([128, 512, 64, 10], dropouts=[0.5, 0.1, -1], activation='relu', norm="wn", update_type="momentum")
+nn = MLP([128, 512, 256, 64, 10], dropouts=[-1, -1, -1, -1], activation='relu', norm="wn", update_type="nes_momentum")
 train_acc, train_loss, test_acc, test_loss = nn.fit(X_train, data.label_dev, X_val, data.label_val, my=0.95,
-                                                    learning_rate=1e-4,
+                                                    learning_rate=1e-5,
                                                     epochs=max_epoch, batchsize=batch_size)
 _, output = nn.predict(X_test)
 
 with h5py.File('../Output/Predicted_labels.h5', 'w') as hf:
     hf.create_dataset("label",  data=output)
 
-prefix = 'layer_3'
+prefix = 'final_result'
 f = open('results_{}.csv'.format(prefix), "w")
 field_names = ['train_acc', 'train_loss', 'test_acc', 'test_loss']
 writer = csv.DictWriter(f,fieldnames=field_names)

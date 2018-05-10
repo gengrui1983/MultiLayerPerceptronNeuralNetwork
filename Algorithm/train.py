7d24ebb12b9543dd8d9701db4087f956a6778a77
chenjialu <jche6589@uni.sydney.edu.au>
2018-05-10 23:46:08 +1000

    add comments for the train file

--- Algorithm/train.py
+++ Algorithm/train.py
@@ -1,27 +1,28 @@
 import csv
 
-from data_loader import data_loader
-from models.networks import MLP
+from Algorithm.data_loader import data_loader
+from Algorithm.models.networks import MLP
 import numpy as np
 
-from models.plot import Plot
-
+from Algorithm.models.plot import Plot
+# loading the data
 data = data_loader.DataLoader(False).load_data()
-
+# calculate mean and standard deviation for preprocessing
 mean = np.mean(data.training_dev, axis=0)
 std = np.std(data.training_dev, axis=0)
-
+# data preprocessing
 X_train = data.training_dev - mean / std
 X_val = data.training_val - mean / std
 
-batch_size = 200
+batch_size = 5
 max_epoch = 300
-
+# Build a Multiple Layer Neural Network
 nn = MLP([128, 512, 64, 10], dropouts=[0.5, 0.1, -1], activation='relu', norm="wn", update_type="nes_momentum")
+# MLP training and validation
 train_acc, train_loss, test_acc, test_loss = nn.fit(X_train, data.label_dev, X_val, data.label_val, my=0.95,
                                                     learning_rate=1e-4,
                                                     epochs=max_epoch, batchsize=batch_size)
-
+# Plotting the result
 prefix = 'layer_3'
 f = open('results_{}.csv'.format(prefix), "w")
 field_names = ['train_acc', 'train_loss', 'test_acc', 'test_loss']
